b8fb696f3e2d403e46e749f946058d2c934ef1a5
Wilson Neto <wilsonpsn@hotmail.com>
2020-11-15 23:55:21 -0300

    Added eslint

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    const timeouter = () => {
+      setSeconds((seconds) => seconds + 1);
+    };
+    if (isRunning) {
+      const timerId = setInterval(timeouter, 1000);
+      setTimer(timerId);
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, []);
+
+  function handleStartTimeout() {
+    setIsRunning(true);
+  }
+
+  function handleResumeTimeout() {
+    setIsRunning(true);
+  }
+
+  function handlePauseTimeout() {
+    setIsRunning(false);
+  }
+
+  function handleClearTimeout() {
+    setIsRunning(false);
+    setSeconds(0);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{seconds}</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={handleStartTimeout}>
+        <Text>Começar contador</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleStartTimeout}>
+        <Text>Pausar contador</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleClearTimeout}>
+        <Text>Parar contador</Text>
+      </TouchableOpacity>
     </View>
   );
 }
